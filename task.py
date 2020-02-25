94dfd4840116e513bfba3b1dbc65daa63c82eef3
Laura Jiang <jianglau@oregonstate.edu>
2020-02-24 18:21:18 -0800

    add date reference

--- task.py
+++ task.py
@@ -12,7 +12,7 @@ def findarea(radius):
 def getitems(l):
     return [l[0], l[-1]]
 
-
+# referenced https://stackoverflow.com/questions/151199/how-to-calculate-number-of-days-between-two-given-dates
 def findDateRange(d1, d2):
     first = datetime.strptime(d1, "%m/%d/%Y")
     second = datetime.strptime(d2, "%m/%d/%Y")
